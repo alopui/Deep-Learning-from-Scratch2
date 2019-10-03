84419399a54fabe7876555f2ad63a494156618d8
Kim Jin Kwan <kkole3897@naver.com>
2019-10-04 01:52:11 +0900

    Chapter7 ver1

--- 김진관/common/layers.py
+++ 김진관/common/layers.py
@@ -163,5 +163,5 @@ class Embedding:
     def backward(self, dout):
         dW, = self.grads
         dW[...] = 0
-        np.scatter_add(dW, self.idx, dout)
+        np.add.at(dW, self.idx, dout)
         return None
